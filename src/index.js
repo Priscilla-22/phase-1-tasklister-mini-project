dd268a8e7eed3395059d33305a1cdd59c5e7f82b
Priscilla-22 <priscillamw123@gmail.com>
2023-12-20 21:51:32 +0300

    Appended a new list created a button and function to delete the items in the lists

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
   // your code here
-});
+  const form = document.getElementById('create-task-form');
+  const taskInput = document.getElementById('new-task-description');
+  const taskList = document.getElementById('tasks');
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // Created a new list item
+    const listItem = document.createElement('li');
+    // Created a delete button
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'Delete';
+    deleteBtn.addEventListener('click', () => {
+      taskList.removeChild(listItem);
+    });
+
+    listItem.textContent = taskInput.value;
+    listItem.appendChild(deleteBtn);
+
+    taskList.appendChild(listItem);
+
+    taskInput.value = ''
+  });
+
+})
+
+
+
+
+  /* document
+    .getElementById('create-task-form')
+    .addEventListener('submit', (e) => {
+      e.preventDefault();
+    });*/
+
+  // function deleteTasks() {
+  //   let tasks
+  // }
